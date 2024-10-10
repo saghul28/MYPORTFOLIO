f75c7d78c422b215545fbb1cd34e7cf4633cd0c6
Saghul ahmed <altsense2024@gmail.com>
2024-10-10 11:39:57 +0530

    first commit

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -19,7 +19,13 @@ const ContactUs = () => {
   return (
     <div className="max-w-md mx-auto p-6 rounded-lg shadow-lg">
       <h2 className="text-2xl font-bold text-center mb-4">Contact Me</h2>
-      <form className="space-y-4" name="contact" data-netlify="true" method='post'>
+      <form
+        className="space-y-4"
+        name="contact"
+        data-netlify="true"
+        method="post"
+      >
+        <input type="hidden" name="contactus" value="contact" />
         <div>
           <label htmlFor="name" className="block font-semibold">
             Name:
