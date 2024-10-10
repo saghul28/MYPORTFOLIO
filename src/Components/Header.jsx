9d488b862b32ed49e5e6e2adfe47f51c6c0bcc40
Saghul ahmed <altsense2024@gmail.com>
2024-10-10 12:17:43 +0530

    first commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -66,14 +66,14 @@ const Header = () => {
               </a>
             </li>
 
-            <li>
+            {/* <li>
               <a
                 href="#contact"
                 className="block py-2 px-3 text-white hover:border-b-2 hover:border-yellow  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0"
               >
                 Contact Me
               </a>
-            </li>
+            </li> */}
           </ul>
         </div>
       </div>
